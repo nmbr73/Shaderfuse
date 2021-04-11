36e9fa9ee8ff6905a984a138ddaae65c0f98668e
nmbr73 <37710085+nmbr73@users.noreply.github.com>
2021-04-11 17:50:22 +0200

    Test Markdown

--- Tools/Scripts/Comp/Shadertoys/Repo Tools/Refresh the Fuse overview MarkDown files.lua
+++ Tools/Scripts/Comp/Shadertoys/Repo Tools/Refresh the Fuse overview MarkDown files.lua
@@ -26,6 +26,10 @@ handle:write([[
 <!--  ALL CHANGES WILL BE OVERWRITTEN WITHOUT ANY FURTHER NOTICE -->
 <!--                                                             -->
 
+<style type="text/css">
+  p.error {color:red;}
+</style>
+
 ]])
 
 handle:write('# Shaders\n\n')
@@ -37,36 +41,47 @@ local currentCategory=''
 for i, fuse in ipairs(fuses.list) do
 
   if fuse.file_category ~= currentCategory then
+
+    if currentCategory~='' then
+      handle:write('</table>\n')
+    end
+
     currentCategory=fuse.file_category
 
     handle:write(
-      "\n\n## "..fuse.file_category.." Shaders\n\n"..
-      "Preview  | Information\n"..
-      "---------|------------\n"
+      "\n\n## "..fuse.file_category.." Shaders\n\n<table>"
+--      .."Preview  | Information\n"
+--      .."---------|------------\n"
     )
 
   end
 
   handle:write(
-      '[!['..fuse.file_category..'/'..fuse.file_fusename..']('..fuse.file_category..'/'..fuse.file_fusename..'_320x180.png)]('..fuse.file_category..'/'..fuse.file_filename..'.md)'
-    ..'|'
+      '<tr>\n  <td>'
+    ..'[!['..fuse.file_category..'/'..fuse.file_fusename..']('..fuse.file_category..'/'..fuse.file_fusename..'_320x180.png)]('..fuse.file_category..'/'..fuse.file_filename..'.md)'
+    ..'</td>\n  <td><p>'..(fuse.error and ':boom:' or ':four_leaf_clover:')..'</p><p>'
     ..'<nobr>Fuse: ['..fuse.file_fusename..']('..fuse.file_category..'/'..fuse.file_fusename..'.md)</nobr><br />'
     ..'<nobr>Category: ['..fuse.file_category..']('..fuse.file_category..'/OVERVIEW.md)</nobr><br />'
     )
 
 
-  if not(fuse.error) then
+  if (fuse.error) then
     handle:write(
         '<nobr>Shadertoy: ['..fuse.shadertoy_name..'](https://www.shadertoy.com/view/'..fuse.shadertoy_id..')</nobr><br />'
       ..'<nobr>Author: ['..fuse.shadertoy_author..'](https://www.shadertoy.com/user/'..fuse.shadertoy_author..')</nobr><br />'
       ..'<nobr>Ported by: ['..fuse.dctlfuse_author..'](../Site/Profiles/'..fuse.dctlfuse_author..'.md)</nobr><br />'
       )
   else
-    handle:write('<span style="color:red; ">'..fuse.error..'</span>')
+    handle:write('</p>\n  <p class="error">'..fuse.error)
   end
 
-  handle:write('\n')
+  handle:write('</p></td>\n</tr>\n')
 
 end
+
+if currentCategory~='' then
+  handle:write('</table>\n')
+end
+
 
 handle:close()
