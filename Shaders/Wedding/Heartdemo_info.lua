acb132847aaccfbd607569b036168bba5807f9e7
it's /me <37710085+nmbr73@users.noreply.github.com>
2022-12-31 19:00:34 +0100

    Tried the conversion for another Fuse ... and it seems to work so far :-)

---
+++ Shaders/Wedding/Heartdemo_info.lua
@@ -0,0 +1,21 @@
+
+info = {
+
+    Shadertoy = {
+        Name = "Heart Demo",
+        Author = "17dpn01",
+        ID = "wttXD7",
+    },
+
+    Fuse = {
+        Author = "JiPi",
+        Date = "2022-12-05"
+    },
+
+    Compatibility = {
+        macOS_Metal    = nil,  -- not tested yet
+        macOS_OpenCL   = nil,  -- not tested yet
+        Windows_CUDA   = true, -- works
+        Windows_OpenCL = true, -- works
+    },
+}
